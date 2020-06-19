c48bc8349e5fc2dc8fe9495bdfde1facf08f652b
renezsz <adrian.r.kulik@gmail.com>
2020-06-19 21:51:26 +0200

    Solution to task 1

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -2,6 +2,9 @@ package bot;
 
 public class SimpleBot {
     public static void main(String[] args) {
-
+        String name = "Hello Bot";
+        int yearOfCreation = 2020;
+        System.out.println("Hello! My name is " + name + ".");
+        System.out.println("I was created in " + yearOfCreation);
     }
 }
